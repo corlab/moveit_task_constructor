77d13b417f7948106474f913ab8aae7eb9beb5ad
Janneke Simmering <jsimmering@techfak.uni-bielefeld.de>
2024-06-13 08:41:07 +0200

    remove cmake-build-debug from git

--- demo/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "moveit_task_constructor_demo"
-PROJECT_SPACE_DIR = "/home/humation/catkin_ws/src/moveit_task_constructor/demo/cmake-build-debug/devel"
-PROJECT_VERSION = "0.1.3"
